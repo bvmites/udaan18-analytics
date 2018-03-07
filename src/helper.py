a51f0ff09485ea5ba0ccfcab3cd71aff6331beeb
Pranshu Dave <pranshudave@gmail.com>
2018-03-07 21:42:32 +0530

    Add method to generate formatted excel sheets

--- src/helper.py
+++ src/helper.py
@@ -1,16 +1,17 @@
 import json
 from os import path
 
+import pandas as pd
 from pymongo import MongoClient
 
 
-def get_mapping():
+def get_mapping(file_name):
     """
     Gets mapping file name and returns a JSON object
     :return: a python dict created from the JSON configuration
     """
     basepath = path.dirname(__file__)
-    file_name = path.abspath(path.join(basepath, "..", "fields_mapping.json"))
+    file_name = path.abspath(path.join(basepath, "..", file_name))
     try:
         with open(file_name) as fields_mapping:
             mapping = json.load(fields_mapping)
@@ -45,3 +46,37 @@ def init_db():
     # Get db
     db = client.udaan18
     return db
+
+
+def write_to_excel(df_list, event_list, start_string):
+    """
+    Helper function to write a list of Pandas dataframe to excel file
+    :param df_list: list of Pandas Dataframe
+    :return:
+    """
+    i = 0
+    print(event_list)
+    for df in df_list:
+        # Shift Index by 1
+        df.index += 1
+        j = 0
+        max_list = []  # List that stores max length of strings in a column
+
+        for col in df.columns.values:
+            max_list.append(
+                max(len(col), df[col].map(str).map(len).max())
+            )
+
+        writer = pd.ExcelWriter(start_string + '_' + str(event_list[i]) + '.xlsx', engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='Sheet1')
+
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+
+        j = 1
+        for m in max_list:
+            worksheet.set_column(j, j, m)
+            j += 1
+
+        i += 1
+        workbook.close()
